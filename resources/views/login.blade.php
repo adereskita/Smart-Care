c3079b56ffc92c5d75bcadcbe10d813edf4ee6f1
adereskita <ade.reskita@yahoo.com>
2019-10-26 17:43:20 +0700

    login page done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -20,6 +20,7 @@
 
     </head>
 <body>
+    <!-- ALERT -->
     @if (session('alert'))
     <div class="uk-alert-danger" uk-alert>
         <a class="uk-alert-close" uk-close></a>
@@ -30,18 +31,11 @@
     <div id="register-card" class="uk-card uk-card-default uk-height-large uk-card-body uk-card-large uk-padding-remove uk-margin-medium-top uk-margin-small-bottom uk-width-3-4@m uk-align-center" uk-grid>
     <!-- <div class="uk-inline"> -->
         <div class="uk-position-left">
-            <h3 id="signup-text" class="uk-card-title uk-margin-medium-top uk-text-center uk-margin-remove-bottom">
-                Create Account
+            <h3 id="signup-text" class="uk-card-title uk-margin-medium-top uk-margin-medium-left uk-margin-remove-bottom">
+                Login
             </h3>
             <form action="" method="">
                 <fieldset id="field-form" class="uk-fieldset uk-width-large uk-padding">
-                    <!-- NAMA -->
-                    <div class="uk-margin">
-                        <div class="uk-inline uk-width-large">
-                            <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: user"></span>
-                            <input class="uk-input" name="name" type="text" placeholder="Name">
-                        </div>
-                    </div>
                     <!-- EMAIL -->
                     <div class="uk-margin">
                         <div class="uk-inline uk-width-large">
@@ -56,11 +50,14 @@
                             <input class="uk-input" name="password" type="password" placeholder="Password">
                         </div>
                     </div>
-                    <button id="btn-log" class="uk-button uk-button-primary uk-width-1-3 uk-padding-remove uk-align-center uk-margin-small">Sign Up</button>
-                        <p class="uk-text-light uk-text-center">
-                            By clicking "Sign up" you are agree with our 
-                            <a href="">Terms & Condition</a>
-                        </p>
+                    <div class="uk-margin">
+                        <a href="#" class="uk-text-light uk-text-center">
+                            forgot your password?
+                        </a>
+                    </div>
+                        
+                    <button id="btn-log" class="uk-button uk-button-primary uk-width-1-3 uk-padding-remove uk-align-center uk-margin-small">Login</button>
+                        
                 </fieldset>
             </form>
         </div>
@@ -68,14 +65,14 @@
                 <div id="color-overlay">
                     
                     <h2 class="uk-text-center uk-text-bold uk-margin-xlarge-top">
-                        Welcome Back!
+                        Hello !
                     </h2>
                     <p class="uk-text-center uk-text-light">
-                        If you already have an account please <br>
-                        login with your personal info
+                        Create your account here <br>
+                        to start a healty journey with us !
                     </p>
-                    <a id="btn-login" href="/login" class="uk-button uk-button-default uk-align-center uk-width-small">
-                        Login
+                    <a id="btn-login" href="/register" class="uk-button uk-button-default uk-align-center uk-width-small">
+                        Sign Up
                     </a>
                 </div>
                 <img class="uk-position-top-right uk-margin-right uk-margin-top" src="/images/smart-care-logo.png" width=32 height=32 alt="">
--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,18 @@
 |
 */
 
+//VIEW ROUTE
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/register', function () {
     return view('register');
 });
-Route::get('/dashboar', function () {
-    return view('dashboar');
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/dashboard', function () {
+    return view('dashboard');
 });
 
 Route::get('/firebase','FirebaseController@index');
